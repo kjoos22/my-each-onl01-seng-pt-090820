91954ee75aa55dbfb710672d9323b732c9fbc235
Kristian Joos <kjoos22@gmail.com>
2020-09-14 22:10:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,10 @@
 def my_each(input)
   x = 0
-  while x <= input.size
-    yield input[x]
-    x += 1
+  if input.size > 0
+    while x <= input.size
+      yield input[x]
+      x += 1
+    end
   end
   
 end
